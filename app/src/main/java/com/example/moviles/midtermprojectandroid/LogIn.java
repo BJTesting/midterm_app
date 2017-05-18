8e31a851d3805ad5cdf5194d1e9f9861b0eae81b
Bryan Fabian Julca Villarreal <bjulcavi@usersad.everis.int>
2017-05-18 14:34:03 -0500

    --

--- app/src/main/java/com/example/moviles/midtermprojectandroid/LogIn.java
+++ app/src/main/java/com/example/moviles/midtermprojectandroid/LogIn.java
@@ -1,17 +1,31 @@
 package com.example.moviles.midtermprojectandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class LogIn extends AppCompatActivity {
-    Button log_in,sign_up;
+    Button log_in,sign_up,forgot_pwd;
     EditText username,pwd;
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        log_in = (Button) findViewById(R.id.log_in);
+        sign_up = (Button) findViewById(R.id.sign_up);
+        forgot_pwd = (Button) findViewById(R.id.forgot_pwd);
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(LogIn.this,SignUp.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 }
